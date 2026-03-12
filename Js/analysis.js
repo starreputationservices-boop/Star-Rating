afae4c314da45f8a2fdb95071fc2109cf57150d4
starreputationservices-boop <starreputationservices@gmail.com>
2026-03-13 01:04:53 +0530

    Create analysis.js

---
+++ Js/analysis.js
@@ -0,0 +1,15 @@
+console.log("Analysis page loaded")
+
+let totalRatings = 120
+let fiveStar = 80
+let fourStar = 20
+let threeStar = 10
+let twoStar = 5
+let oneStar = 5
+
+document.getElementById("total").innerText = totalRatings
+document.getElementById("five").innerText = fiveStar
+document.getElementById("four").innerText = fourStar
+document.getElementById("three").innerText = threeStar
+document.getElementById("two").innerText = twoStar
+document.getElementById("one").innerText = oneStar
